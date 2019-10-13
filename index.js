befe05bb39946325623a13eca1e27a73f9504078
tylerccaceres <tyler.caceres@gmail.com>
2019-10-13 10:07:24 -0400

    change features attributes

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-const multipleFaker = require("multiple-faker");
+const faker = require("faker");
 
-const table = "users";
+const tableName = "users";
 const numRows = 25;
 const fields = ["first_name", "last_name", "job_title", "phone_number", "random_number"];
-const attributes = {
-  name: ["firstName", "lastName", "jobTitle"],
-  phone: ["phoneNumber"],
-  random: ["number"]
+const attributes = [
+  "name.firstName",
+  "name.lastName",
+  "name.jobTitle",
+  "phone.phoneNumber",
+  "random.number"
+];
+
+const seedStart = (tableName, fields) => {
+  const numFields = fields.length;
+  let SQLstart = `INSERT INTO ${tableName} (`;
+  for (let i = 0; i < numFields; i++) {}
 };
 
-const seedDataSQL = (table, fields, attributes) => {
-  const seed = ``;
-  seed += `insert into ${table} (`;
-  seed += fields.join(", ");
-  seed += `) values (`;
-  seed += `);`;
+const seedDataSQL = (numRows, attributes) => {
+  let query = "";
+  let queryString = "";
+  const numAttributes = attributes.length;
+  for (let i = 0; i < numRows; i++) {
+    queryString += "(";
+    for (let j = 0; j < numAttributes; j++) {
+      let value = eval(`faker.${attributes[j]}`)();
+      console.log("value : ", typeof value);
+      let extraChar = typeof value === "number" ? "" : '"';
+      queryString += extraChar;
+      queryString += value;
+      queryString += extraChar;
+      queryString += numAttributes - 1 === j ? "" : ", ";
+    }
+    queryString += "), ";
+    query += queryString;
+    queryString = "";
+  }
+  console.log(query);
 };
 
-const generatedData = multipleFaker(numRows, attributes);
+seedDataSQL(numRows, attributes);
