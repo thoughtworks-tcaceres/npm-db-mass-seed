d20e34184c4bd145d53c3f1eae08ffb87681bce2
tylerccaceres <tyler.caceres@gmail.com>
2019-10-13 11:54:54 -0400

    start to refactor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const faker = require("faker");
 
 const tableName = "users";
-const numRows = 25;
+const numRows = 100;
 const fields = ["first_name", "last_name", "job_title", "phone_number", "random_number"];
 const attributes = [
   "name.firstName",
@@ -11,13 +11,38 @@ const attributes = [
   "random.number"
 ];
 
-const seedStart = (tableName, fields) => {
-  const numFields = fields.length;
-  let SQLstart = `INSERT INTO ${tableName} (`;
-  for (let i = 0; i < numFields; i++) {}
+const isValidTableName = (tableName) => {
+  return typeof tableName === "string" ? true : false;
 };
 
-const seedDataSQL = (numRows, attributes) => {
+const isValidNumRows = (numRows) => {
+  return Number.isInteger(numRows) ? true : false;
+};
+
+const isValidFields = (fields) => {
+  for (let i = 0; i < fields.length; i++) {
+    if (typeof fields[i] !== "string") {
+      return false;
+    }
+  }
+  return true;
+};
+
+const isValidAttributes = (attributes) => {
+  for (let i = 0; i < attributes.length; i++) {
+    if (!eval(`faker.${attributes[i]}`)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const seedStart = (tableName, fields) => {
+  let SQLstart = `INSERT INTO ${tableName} (${fields.join(", ").replace('"', "")}) VALUES `;
+  return SQLstart;
+};
+
+const seedValues = (numRows, attributes) => {
   let query = "";
   let queryString = "";
   const numAttributes = attributes.length;
@@ -25,18 +50,30 @@ const seedDataSQL = (numRows, attributes) => {
     queryString += "(";
     for (let j = 0; j < numAttributes; j++) {
       let value = eval(`faker.${attributes[j]}`)();
-      console.log("value : ", typeof value);
       let extraChar = typeof value === "number" ? "" : '"';
       queryString += extraChar;
       queryString += value;
       queryString += extraChar;
       queryString += numAttributes - 1 === j ? "" : ", ";
     }
-    queryString += "), ";
+    queryString += numRows - 1 === i ? ")" : "), ";
     query += queryString;
     queryString = "";
   }
-  console.log(query);
+  return query;
 };
 
-seedDataSQL(numRows, attributes);
+const generateSQLsyntax = (tableName, numRows, fields, attributes) => {
+  const validAttribute = isValidAttributes(attributes);
+  const validFields = isValidFields(fields);
+  const validNumRows = isValidNumRows(numRows);
+  const validTableName = isValidTableName(tableName);
+
+  if (validAttribute && validFields && validNumRows && validTableName) {
+    return seedStart(tableName, fields) + seedValues(numRows, attributes);
+  } else {
+    throw new Error("Invalid Data Entered. Cannot generate SQL.");
+  }
+};
+
+console.log(generateSQLsyntax(tableName, numRows, fields, attributes));
